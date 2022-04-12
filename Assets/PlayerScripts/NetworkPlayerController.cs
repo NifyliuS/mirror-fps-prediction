01d6e7b9ee840b275e1bbb0b7a5227b09026bb91
gamerguardio <niftylius@gmail.com>
2022-04-12 23:29:10 +0300

    Added basic things in preperation for NetworkPlayerController ( abstraction layer )

--- Assets/PlayerScripts/NetworkPlayerController.cs
+++ Assets/PlayerScripts/NetworkPlayerController.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
 namespace PlayerScripts {
-  public class NetworkPlayerController : NetworkBehaviour { }
+  public class NetworkPlayerController : NetworkBehaviour {
+  #region Private State
+    // Private state variables are set here
+    private bool _isServerLocalPlayer  = false;
+    private bool _isServerRemotePlayer = false;
+    private bool _isClientLocalPlayer  = false;
+    private bool _isClientRemotePlayer  = false;
+  #endregion
+
+    private void Awake() {
+      // For ease of use later i am setting the variables we will need to differentiate the 4 cases of players
+      _isServerLocalPlayer = isServer && isLocalPlayer;
+      _isServerRemotePlayer = !_isServerLocalPlayer;
+      _isClientLocalPlayer = isClient && isLocalPlayer;
+      _isClientRemotePlayer = !_isClientLocalPlayer;
+    }
+    
+  }
 }
